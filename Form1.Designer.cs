ef222dee4d2ea6db4e5d9ba5aa9ae05b20e04f63
olegBsam <olegoleg.ru@mail.ru>
2020-03-08 15:41:08 +0400

    #init first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MagDiplom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.awdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.awdawdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.Location = new System.Drawing.Point(371, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Location = new System.Drawing.Point(415, 261);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.awdToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // awdToolStripMenuItem
+            // 
+            this.awdToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.awdawdToolStripMenuItem});
+            this.awdToolStripMenuItem.Name = "awdToolStripMenuItem";
+            this.awdToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.awdToolStripMenuItem.Text = "awd";
+            // 
+            // awdawdToolStripMenuItem
+            // 
+            this.awdawdToolStripMenuItem.Name = "awdawdToolStripMenuItem";
+            this.awdawdToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.awdawdToolStripMenuItem.Text = "awdawd";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+       
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem awdToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem awdawdToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MagDiplom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.DragDrop += Button1_DragDrop;
+            button1.MouseDown += Button1_MouseDown;
+            DragEnter += Form1_DragEnter;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            var form = sender as Control;
+            var type =(Button) e.Data.GetData(typeof(Button));
+            type.Location = new Point(Cursor.Position.X - form.Location.X,Cursor.Position.Y - form.Location.Y);
+        }
+
+        private void Button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            DoDragDrop(sender as Control, DragDropEffects.Move);
+        }
+
+        private void Button1_DragDrop(object sender, DragEventArgs e)
+        {
+            var g = 12;
+        }
+
+    }
+}
